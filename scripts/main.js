d0231a28e8a9c2097afa94725cbcdebeab5f7da5
Jonathan Bonano <jon_bonano@zoho.com>
2019-11-20 16:28:10 -0600

    This is my real first commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'You are duh bomb, ' + myName;
+  }
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'You are duh bomb, ' + storedName;
+  }
+
+myButton.onclick = function() {
+    setUserName();
+  }
